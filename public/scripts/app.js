39af629d24a20bac680aac7ec82c656dcc9224bc
jashan <jashannoor2001@gmail.com>
2023-04-14 21:55:24 -0400

    Added Random Number Function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,14 @@
-
-const socket=io("http://localhost:8000/")
+const socket = io("http://localhost:8000/")
 
 socket.on('connection');
 
+let button=document.querySelector('button');
+let output=document.querySelector('label')
+function genrateRandom(){
+    let randomNumber=Math.floor(Math.random() * 11);
+    console.log(randomNumber)
+    output.innerText=`Random Number: ${randomNumber}`
+}
 
-
+button.addEventListener('click',genrateRandom);
 
