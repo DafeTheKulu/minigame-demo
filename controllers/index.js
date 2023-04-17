2c651037f872506879d6691adc97177d8b2f7d9e
13r0hit <rohit.rohit@mygeorgian.ca>
2023-04-16 21:30:57 -0400

    created files for homepage and about page

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET homepage page. */
 router.get('/', function(req, res, next) {
+  
+  res.render('homepage', { title: 'click to play' });
+});
+
+/* GET game page. */
+router.get('/game', function(req, res, next) {
   
   res.render('index', { title: 'Number.io' });
 });
+/* GETabout page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about team' });
+});
+
 
 module.exports = router;
