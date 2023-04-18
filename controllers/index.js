f0f764ff3f6d20995eca2876f3ee9ca0c0902ff9
mcgmark <mcgmark14@icloud.com>
2023-04-18 18:02:17 -0400

    Refactored game logic for server-side

--- controllers/index.js
+++ controllers/index.js
@@ -3,29 +3,18 @@ const router = express.Router();
 
 /* GET homepage page. */
 router.get('/', function(req, res, next) {
-  
   res.render('homepage', { title: 'click to play' });
 });
 
 /* GET game page. */
 router.get('/game', function(req, res, next) {
-  
-  res.render('index', { title: 'Number.io' });
+  res.render('game', { title: 'Number.io' });
 });
+
 /* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'about team' });
 });
 
-
-/* GET WIN page. */
-router.get('/win', function(req, res, next) {
-  res.render('win', { title: 'YOU WIN' });
-});
-
-/* GET LOSE page. */
-router.get('/lose', function(req, res, next) {
-  res.render('lose', { title: 'YOU LOSE' });
-});
 
 module.exports = router;
---
+++ public/scripts/client.js
@@ -0,0 +1,56 @@
+// Connect client to the websocket server (same host as default)
+const socket = io(); 
+
+
+// USERNAME INPUT
+
+// Initialize variable objects for dom elements
+const nameInput = document.querySelector('#nameInput'); //username input
+const buttonName = document.querySelector('#buttonName'); //
+
+// Event listener to submit username to server.
+buttonName.addEventListener('click', () => {
+    const name = nameInput.value;
+    socket.emit('setUsername', name); // emit a socket.io event with the name data
+});
+
+
+// GAME INTERFACE //
+
+// Initialize variable objects for dom elements
+let guessInput = document.querySelector(".guessInput");
+let outputText = document.querySelector('.output');
+let buttonGuess = document.querySelector(".buttonGuess");
+let buttonRestart = document.querySelector(".buttonRestart");
+let prevGuess = document.querySelector(".prevGuess");
+
+// Game button event listeners
+buttonGuess.addEventListener("click", () => {
+    const guessNumber = guessInput.value;
+    prevGuess.textContent = prevGuess.textContent + ' ' + guessNumber;
+    socket.emit('guess', guessNumber); // emit a socket.io event with the guessNumber to the server for checking
+});
+
+buttonRestart.addEventListener("click", () => {
+    parent.location = '/';
+});
+
+
+// SOCKETIO CLIENT RECEIVE FROM SERVER //
+
+// When play message is recieved swap div display
+socket.on("play", () => {
+    document.querySelector('#name-input-container').classList.toggle('d-block');
+    document.querySelector('#name-input-container').classList.toggle('d-none');
+    document.querySelector('#game-interface-container').classList.toggle('d-none');
+});
+
+socket.on("correct", () => {
+    outputText.textContent = "You win";
+});
+
+socket.on("incorrect", (message) => {
+    outputText.textContent = message;
+});
+
+
