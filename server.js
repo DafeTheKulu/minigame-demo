c2db9e41227b36cb8a24574fb2adf758b6b614ac
David Janssen <djanssen2112@gmail.com>
2023-04-20 20:05:53 -0400

    Changed first half of demo code

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
-// Modules 
-const express = require('express');  //Add express to project
+// Modules
+const express = require("express"); //Add express to project
 const app = express(); // Create new express app object
-const http = require('http'); // Add HTTP server/client
-const path = require("path"); // Add path 
-
-
+const http = require("http"); // Add HTTP server/client
+const path = require("path"); // Add path
 
 //
 //      SOCKET IO & GAME SETUP
 //
 /////////////////////////////////////////
 
-// Create http server/client attached to app object
-const server = http.createServer(app); 
+/* -------- STEP 1: Setup the server--------*/
 
-// Require Server from socket.io  
-const { Server } = require("socket.io"); 
+// Create http server/client attached to app object
+
+// Require Server from socket.io
 
 // Create new socket.io server and call it io
-const io = new Server(server); 
 
 // Object to store individual player details on server-side
-let players = {}; 
 
 //////////////////////////////////////////
 
-
-
 //
-//     EXPRESS APP SETUP 
+//     EXPRESS APP SETUP
 //
 //////////////////////////////////////////
-
 
 // Require controllers
 const index = require("./controllers/index");
@@ -49,27 +42,24 @@ app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 server.listen(3000, () => {
-  console.log('Game ServerIO Active!')
-})
+	console.log("Game ServerIO Active!");
+});
 /////////////////////////////////////////////////////////////
-
-
-
 
 //
 //   SOCKETIO FUNCTIONS
@@ -79,63 +69,68 @@ server.listen(3000, () => {
 // CONNECTION - When connection message received grab player socket information
 // This allows the application to communicate with specific sockets/users/devices
 io.on("connection", (socket) => {
+	// Initialize variables to store id of current player socket client
+	const playerId = socket.id;
 
-  // Initialize variables to store id of current player socket client
-  const playerId = socket.id;
+	// Server player connected message
+	console.log("Socket Connected: " + playerId);
 
-  // Server player connected message 
-  console.log('Socket Connected: ' + playerId); 
+	/* -------- STEP 2a: Prepare the socket to respond when it receives a username -----------*/
+	// SETUSERNAME -  When 'setUsername' message received, generate random number, and store it to variable for specific user,
+	// emit 'play' message to that socket only
+	socket.on("setUsername", (username) => {
+		/* -------------Step 2b: Assign a random number to the new user -------------*/
+		if (!players[playerId]) {
+			let randomNumber = Math.floor(Math.random() * 10) + 1; // generate random number 1 - 10
+			players[playerId] = {
+				username: username,
+				randomNumber: randomNumber,
+			};
+			/*--------Step 2c: Set username and their puzzle random number to object*/
+			console.log(
+				"New player added " +
+					username +
+					" : " +
+					playerId +
+					" : " +
+					randomNumber
+			);
+			/* ------Step 2d: Emit play message to specific socket/user */
+			socket.emit("play");
+		}
+	});
 
+	// GUESS - When 'guess' message received compare guessNumber to randomNumber and emit proper message
+	socket.on("guess", (guessNumber) => {
+		const player = players[playerId]; // set current player details to get player's randomNumber
+		const randomNumber = player.randomNumber; // set to current socket/player random number to compare
 
+		/* ----------- Step 3: Compare player's guessNumber to player's randomNumber using emit() ---------*/
+		if (guessNumber == randomNumber) {
+			socket.emit("correct"); // emit correct message
+		} else if (guessNumber > randomNumber) {
+			socket.emit("incorrect", "Too high!"); // emit incorrect message with string
+		} else if (guessNumber < randomNumber) {
+			socket.emit("incorrect", "Too low!"); // emit incorrect message with string
+		} else {
+			socket.emit("incorrect"); // emit incorrect message
+		}
+	});
 
-  // SETUSERNAME -  When 'setUsername' message received, generate random number, and store it to variable for specific user, 
-  // emit 'play' message to that socket only
-  socket.on('setUsername', (username) => {
-    if (!players[playerId]) {
-      let randomNumber = Math.floor(Math.random() * 10) + 1; // generate random number 1 - 10
-      players[playerId] = { username: username, randomNumber: randomNumber }; // set username and their puzzle random number to object
-      console.log('New player added ' + username + ' : ' + playerId + ' : ' + randomNumber);
-
-      // PLAY - emit play message to specific socket/user
-      socket.emit("play"); 
-    }
-  });
-
-
-
-  // GUESS - When 'guess' message received compare guessNumber to randomNumber and emit proper message
-  socket.on('guess', (guessNumber) => {
-    const player = players[playerId]; // set current player details to get player's randomNumber
-    const randomNumber = player.randomNumber; // set to current socket/player random number to compare
-
-    // compare player's guessNumber to player's randomNumber 
-    if (guessNumber == randomNumber) {
-      socket.emit('correct'); // emit correct message
-    } else if (guessNumber > randomNumber) {
-      socket.emit('incorrect', 'Too high!') // emit incorrect message with string
-    } else if (guessNumber < randomNumber) {
-      socket.emit('incorrect', 'Too low!') // emit incorrect message with string
-    } else {
-      socket.emit('incorrect'); // emit incorrect message
-    }
-  });
-
-
-
-  // DISCONNECT - When 'disconnect' message received check if user made username and remote socket/player from object
-  socket.on("disconnect", () => {
-    if (players[playerId]) { // Socket connected but did user create a username? 
-      const playerName = players[playerId].username;
-      console.log('User Disconnected: ' + ' ' + playerName + ' : ' + playerId);
-      delete players[playerId];
-    } else {
-      console.log('User Disconnected: ' + playerId);
-    }
-
-  });
-
+	// DISCONNECT - When 'disconnect' message received check if user made username and remote socket/player from object
+	socket.on("disconnect", () => {
+		if (players[playerId]) {
+			// Socket connected but did user create a username?
+			const playerName = players[playerId].username;
+			console.log(
+				"User Disconnected: " + " " + playerName + " : " + playerId
+			);
+			delete players[playerId];
+		} else {
+			console.log("User Disconnected: " + playerId);
+		}
+	});
 });
-
 
 //Export
 module.exports = app;
